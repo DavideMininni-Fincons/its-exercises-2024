1c98aec0b9080ead606ea0a2aaad31d7c80c9703
Davide Mininni <davide.mininni@finconsgroup.com>
2024-05-20 20:25:29 +0200

    fix: minor changes

--- exercises/4_guessRandom2/script.js
+++ exercises/4_guessRandom2/script.js
@@ -2,20 +2,19 @@ let attempts = 1, min = 1, max = 100;
 const randomNumber = Math.floor(Math.random() * (max - min + 1)) + min;
 alert(randomNumber);
 
-let guess;
+let guess = parseInt(prompt("I'm thinking of a number between 1 and 100. Can you guess what it is?"));
 
 // Check if the guess is correct
 do {
-    guess = parseInt(prompt("I'm thinking of a number between 1 and 100. Can you guess what it is?"));
-
-    attempts++;
-
     // Provide hints to the player
     if (guess > randomNumber) {
         guess = parseInt(prompt("Too high! Try guessing lower."));
     } else if (guess < randomNumber) {
         guess = parseInt(prompt("Too low! Try guessing higher."));
+    } else {
+        break;
     }
+    attempts++;
 } while (guess !== randomNumber);
 
 // Congratulate the player on guessing the correct number
