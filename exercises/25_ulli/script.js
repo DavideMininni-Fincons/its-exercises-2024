4734faca84d51b471f3a16e81caab6986c8a3969
Davide Mininni <davide.mininni@finconsgroup.com>
2024-05-27 13:49:40 +0200

    fix: minor fixes, wrong exercise

--- exercises/25_ulli/script.js
+++ exercises/25_ulli/script.js
@@ -1,41 +1,40 @@
-const dropdownButton = document.querySelector('.dropdown-button');
-const dropdownOptions = document.querySelector('.dropdown-options');
-const selected = document.querySelector('.selectedElement');
-const options = document.querySelectorAll('li');
-
-dropdownButton.addEventListener('click', () => {
-    toggleDisplay();
-});
-
-dropdownOptions.addEventListener('click', (e) => {
-    if (e.target.tagName !== 'LI') {
+const ul = document.querySelector('#ul');
+const li = document.querySelectorAll('li');
+ul.addEventListener('click', (e) => {
+    if (e.target.tagName !== "LI")
         return;
+
+    if (e.ctrlKey || e.metaKey) {
+        toggleSelect(e.target);
+    } else {
+        singleSelect(e.target);
     }
-    const sel = document.createElement('div');
-    sel.innerText = e.target.innerText
-    selected.append(sel);
-    toggleDisplay();
 });
 
-dropdownButton.addEventListener('keyup', (e) => {
-    if (e.key === 'ArrowDown' || e.key === 'ArrowUp') {
-        const focused = Array.from(options).findIndex(e => e.classList.contains('focused'));
-        if (focused === -1 || focused === options.length - 1) {
-            options[0].classList.add('focused');
-            options[options.length - 1].classList.remove('focused');
-        } else {
-            options[focused].classList.remove('focused');
-            const next = e.key === 'ArrowDown' ? +1 : -1;
-            options[focused + next].classList.add('focused');
+// prevent unneeded selection of list elements on clicks
+ul.addEventListener('mousedown', () => {
+    return false;
+});
+
+li.forEach((listitem) => {
+    listitem.addEventListener('mouseenter', (e) => {
+        if (!e.target.classList.contains('selected')) {
+            e.target.classList.add('enter');
         }
-    }
-});
-
-function toggleDisplay() {
-    dropdownOptions.classList.toggle('show-panel');
-    resetFocused();
+    });
+    listitem.addEventListener('mouseleave', (e) => {
+        e.target.classList.remove('enter');
+    });
+})
+function toggleSelect(li) {
+    li.classList.toggle('selected');
 }
 
-function resetFocused() {
-    Array.from(options).forEach(e => e.classList.remove('focused'));
+function singleSelect(li) {
+    let selected = ul.querySelectorAll('.selected');
+    for(let elem of selected) {
+        elem.classList.remove('selected');
+    }
+    li.classList.remove('enter');
+    li.classList.add('selected');
 }
